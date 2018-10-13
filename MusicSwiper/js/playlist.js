dc5f4f22e23a0ef0b9ce2b57d119226b956deafb
mohsr <mkrizvi98@gmail.com>
2018-10-13 09:52:57 -0400

    added function to create playlists

---
+++ MusicSwiper/js/playlist.js
@@ -0,0 +1,50 @@
+    createAndPopulatePlaylist = async (tracks, user_id, access_token) => {
+        var xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = (e) => {
+            if (xhr.readyState !== 4) {
+                return;
+            }
+            if (xhr.status == 200) {
+                var data = xhr.responseText;
+                var obj = JSON.parse(data.replace(/\r?\n|\r/g, ''));
+
+                this.populatePlaylist(tracks, obj.id, user_id, access_token);
+            }
+        }
+
+        xhr.open("POST", "https://api.spotify.com/v1/users/" + user_id + 
+                         "/playlists");
+        xhr.setRequestHeader("Authorization", "Bearer " + access_token);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send('{"name": "' + name + '", "description": "' + desc + '}');
+    }
+
+    populatePlaylist = async (tracks, playlist_id, user_id, access_token) => {
+        var xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = (e) => {
+            if (xhr.readyState !== 4) {
+                return;
+            }
+            if (xhr.status == 200) {
+                var data = xhr.responseText;
+                var obj = JSON.parse(data.replace(/\r?\n|\r/g, ''));
+
+                if (this.state.rights.length <= 100) {
+                    Alert.alert("Songs added!");
+                } else {
+                    Alert.alert("100 songs added!");
+                }
+            }
+        }
+
+        /* Grab the first 100 elements. */
+        var data = this.state.rights.slice(0, 100);
+
+        xhr.open("POST", "https://api.spotify.com/v1/playlists/" + playlist_id +
+                         "/tracks");
+        xhr.setRequestHeader("Authorization", "Bearer " + access_token);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify(data));
+    }
